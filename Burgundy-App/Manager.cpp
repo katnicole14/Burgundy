6e8d8f110f1d40a1274340896d8466bfc8c64f25
Siyamthanda Ndlovu <99127918+thunderain@users.noreply.github.com>
2023-11-05 14:54:18 +0200

    Update Manager.cpp

--- Burgundy-App/Manager.cpp
+++ Burgundy-App/Manager.cpp
@@ -3,3 +3,23 @@
 void Manager::construct(int numCustomers) {
     builder->buildPart(numCustomers);
 }
+
+void Manager::observeSatisfaction(std::vector<Table*>& tables) {
+ 	int numTables = tables.size();
+
+    this->observerState = true;
+  // run for loop from 0 to vecSize
+  for( int i = 0; i < numTables; i++)
+  {
+        if (!tables[i]->getTableSatisfaction()) {
+            this->observerState = false;
+        }
+    }
+
+    if (this->observerState) {
+        std::cout << "Manager Observer Result: Restaurant is satisfied" <<std::endl;}
+    else {
+        std::cout << "Manager Observer Result: Restaurant is not satisfied" <<std::endl;
+    }
+
+}
