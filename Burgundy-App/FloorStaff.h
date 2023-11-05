3e2334fd90c9cffdc0e905c7c450594c8939afe3
Siyamthanda Ndlovu <99127918+thunderain@users.noreply.github.com>
2023-11-05 15:14:43 +0200

    Update FloorStaff.h

--- Burgundy-App/FloorStaff.h
+++ Burgundy-App/FloorStaff.h
@@ -1,9 +1,20 @@
 #ifndef FLOORSTAFF_H
 #define FLOORSTAFF_H
 
+#include <iostream>
+#include <vector>
+
+class Restaurant;
+class Table;
 
 class FloorStaff {
+protected:
+	Restaurant* restaurant;
+	bool observerState;
 
+public:
+	FloorStaff();
+	virtual void observeSatisfaction(std::vector<Table*>& tables)=0;
 };
 
 
