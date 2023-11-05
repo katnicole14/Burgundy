c0f57b05d13dbd9f654bf068bce617feb3144782
Siyamthanda Ndlovu <99127918+thunderain@users.noreply.github.com>
2023-11-05 14:55:55 +0200

    Update Manager.h

--- Burgundy-App/Manager.h
+++ Burgundy-App/Manager.h
@@ -1,15 +1,19 @@
 #ifndef MANAGER_H
 #define MANAGER_H
+#include <vector>
+#include "FloorStaff.h"
+#include "Table.h"
 
 #include "AbstractBuilder.h"
 
-class Manager {
+class Manager : public FloorStaff {
 private:
     AbstractBuilder* builder;
 
 public:
     Manager(AbstractBuilder* b) : builder(b) {}
     void construct(int numCustomers);
+    void observeSatisfaction(std::vector<Table*>& tables);
 };
 
 
