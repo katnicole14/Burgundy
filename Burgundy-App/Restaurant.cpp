95ffe80097368fa154b7d8a8dcedd5627e1e7ad2
Siyamthanda Ndlovu <99127918+thunderain@users.noreply.github.com>
2023-11-05 14:57:55 +0200

    Update Restaurant.cpp

--- Burgundy-App/Restaurant.cpp
+++ Burgundy-App/Restaurant.cpp
@@ -3,19 +3,9 @@
 Restaurant::Restaurant() {
 }
 
-
 void Restaurant::attachObserver(FloorStaff* staffMember) {
 	this->observerList.push_back(staffMember);
 }
-
-/* void Restaurant::remove(FloorStaff* staffMember) {
-
-for (FloorStaff* staff : this->observerList) {
-        if (this->observerList[staff] == staffMember) {
-		}
-    }
-}
-*/
 
 void Restaurant::notify(std::vector<Table*>& tables) {
 	for (FloorStaff* staff : this->observerList) {
