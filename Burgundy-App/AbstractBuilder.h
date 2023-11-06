b4096c203e9e154703b24327564bf2234636c9c6
Brenden1998 <50359806+Brenden1998@users.noreply.github.com>
2023-11-06 15:52:11 +0200

    Doxygen comments for AbstractBuilder

--- Burgundy-App/AbstractBuilder.h
+++ Burgundy-App/AbstractBuilder.h
@@ -1,9 +1,17 @@
 #ifndef ABSTRACTBUILDER_H
 #define ABSTRACTBUILDER_H
 
-
+/**
+* \class AbstractBuilder
+* \brief An abstract class that defines the method that the ConcreteBuilder uses.
+*/
 class AbstractBuilder {
 public:
+     /**
+     * \brief Definition of the buildPart function that the ConcreteBuilder will use to make tables.
+     *
+     * \param numCustomers The number of customers that entered the restaurant.
+     */
      virtual void buildPart(int numCustomers) = 0;
 };
 
