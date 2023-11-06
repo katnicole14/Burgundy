075257c28f592398cdceb34f429d539c7a333eb1
Brenden1998 <50359806+Brenden1998@users.noreply.github.com>
2023-11-06 17:18:44 +0200

    Doxygen comments for Customer

--- Burgundy-App/Customer.h
+++ Burgundy-App/Customer.h
@@ -3,21 +3,52 @@
 
 #include <string>
 #include <iostream>
-
+/**
+* \class Customer
+* \brief The customers will be seated at a table and will be able to place orders.
+*/
 class Customer {
 private:
+	/**
+	* \var string customerName
+ 	* \brief The customer's name
+	*/
     std::string customerName;
+	/**
+	* \var int order[8]
+ 	* \brief An array that will hold 8 integers. The integers correspond to menu items that the customer has chosen to order.
+	*/
     int order[8];
     int seatNumber;
 
 public:
+	/**
+	* \brief The constructor for the Customer class.
+ 	* The constructor will initialise the customer's name and set their initial seat number to -1.
+  	* \param customerName The name of the customer.
+	*/
     Customer(std::string customerName);
+	/**
+	* \brief The destructor for the Customer class.
+	*/
     ~Customer();
+	/**
+	* \brief This function will set the customer's order to the passed in array.
+ 	* \param order The order of the customer.
+	*/
     void setOrder(int order[8]);
+	/**
+	* \brief This function gets the customer's order.
+ 	* \return The customer's order.
+	*/
     int* getOrder();
+	/**
+	* \brief This function gets the customer's name.
+ 	* \return The customer's name.
+	*/
     std::string getCustomerName();
-	int getSeatNumber();
-	void assignSeatNumber(int);
+    int getSeatNumber();
+    void assignSeatNumber(int);
 };
 
 
