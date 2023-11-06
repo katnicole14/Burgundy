678d337efbcfa583ef2ac1475b8038191284bcae
Siyamthanda Ndlovu <99127918+thunderain@users.noreply.github.com>
2023-11-06 23:52:55 +0200

    Update Unoccupied.h Doxygen

--- Burgundy-App/Unoccupied.h
+++ Burgundy-App/Unoccupied.h
@@ -2,6 +2,10 @@
 #define UNOCCUPIED_H
 #include "TableState.h"
 class Table;
+/**
+* \class Unoccupied
+* \brief This class represents the state of the table when it is unoccupied.
+*/
 class Unoccupied :public TableState {
  public :
   virtual void changeTo(Table *c);
