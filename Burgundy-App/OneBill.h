9520c1a1adeb4b5f63e5a10079243b316b196688
Siyamthanda Ndlovu <99127918+thunderain@users.noreply.github.com>
2023-11-07 00:24:09 +0200

    Update OneBill.h Doxygen

--- Burgundy-App/OneBill.h
+++ Burgundy-App/OneBill.h
@@ -1,9 +1,11 @@
 #ifndef ONEBILL_H
 #define ONEBILL_H
 #include "Payment.h"
+/**
+* \class OneBill
+* \brief This class is a strategy to pay the bill in full
+*/
 class OneBill : Payment {
-
-
 public:
 	void pay();
 };
