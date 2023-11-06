9c26e17dc0b742349201789654162b788a61e49e
lerato15240 <147916325+lerato15240@users.noreply.github.com>
2023-11-06 20:20:29 +0200

    doxygen comments update

--- Burgundy-App/Drink.h
+++ Burgundy-App/Drink.h
@@ -1,12 +1,23 @@
 #ifndef DRINK_H
 #define DRINK_H
-
+	/**
+	 * \brief Interface defines the print function that can be altered by the types of drinks
+	 */
 class Drink {
 
 public:
+	/**
+ 	* \brief The constructor for the Drink class.
+  	*/
 
 	Drink();
+	/**
+	 * \brief prints
+	 */
 	virtual void print();
+	/**
+ 	* \brief The destructor for the Drink class.
+  	*/
 	virtual ~Drink();
 };
 
