2fa9b362bb639e29ef55b0a2e437cc0706cdfe67
lerato15240 <147916325+lerato15240@users.noreply.github.com>
2023-11-06 22:20:51 +0200

    doxygen comments update

--- Burgundy-App/SauceChef.h
+++ Burgundy-App/SauceChef.h
@@ -3,13 +3,30 @@
 #include"Chef.h"
 #include"Order.h"
 #include"BurgundySauce.h"
-
+/**
+ * \class SauceChef
+ * \brief A concrete handler that can handle specific requests.
+ */
 class SauceChef : public Chef {
 
 public:
+	/**
+	* \brief SauceChef constructor  
+	*/
 	SauceChef(Chef* next);
+	/**
+	* \brief return the next handler in the chain    
+	*/
 	Chef* getNext();
+	/**
+	* \brief sets the next handler in the chain    
+	*/
 	void setNext(Chef* next);
+	/**
+	* 
+	* \brief Handle the request if it's within this handler's capability, or pass it to the next handler.
+	* \param item The request to be handled.
+	*/
 	void addOrderItem(Order* item);
 	
 };
