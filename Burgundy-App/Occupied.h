b05ef498bd34ab27f11db39ee53e73b25c0b4e6e
Brenden1998 <50359806+Brenden1998@users.noreply.github.com>
2023-11-07 00:25:42 +0200

    Update Occupied.h

--- Burgundy-App/Occupied.h
+++ Burgundy-App/Occupied.h
@@ -10,7 +10,15 @@ class Table;
 class Occupied : public TableState {
 
   public :
+  /**
+  * \brief This function changes the state of the table to BillPaid.
+  * \param c A pointer to a table.
+  */
   virtual void changeTo(Table *c);
+  /**
+  * \brief This function returns the string "Occupied".
+  * \return The string "Occupied".
+  */
   virtual std::string getState();
 };
 
