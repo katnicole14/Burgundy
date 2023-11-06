413d7d454f7cebe59a3a731e2a8770b9b72810c8
Brenden1998 <50359806+Brenden1998@users.noreply.github.com>
2023-11-06 23:32:53 +0200

    Doxygen comments for BillPaid

--- Burgundy-App/BillPaid.h
+++ Burgundy-App/BillPaid.h
@@ -2,10 +2,21 @@
 #define BILLPAID_H
 #include "TableState.h"
 class Table;
-
+/**
+* \class BillPaid
+* \brief This class represents the state of the table when the bill has been paid.
+*/
 class BillPaid :public TableState {
   public :
+  /**
+  * \brief This function changes the table to the Dirty state.
+  * \param c A pointer to a table.
+  */
   virtual void changeTo(Table *c);
+  /**
+  * \brief this function returns the string "BilledPaid".
+    \return The string "BilledPaid".
+  */
   virtual std::string getState();
 };
 
