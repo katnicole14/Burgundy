bded40e66e7819deedba8275e3c402af63b7596c
lerato15240 <147916325+lerato15240@users.noreply.github.com>
2023-11-06 21:53:03 +0200

    Update Tomato.h

--- Burgundy-App/Tomato.h
+++ Burgundy-App/Tomato.h
@@ -1,7 +1,10 @@
 #ifndef TOMATO_H
 #define TOMATO_H
 #include"Ingredient.h"
-
+/**
+ * \class Water
+ * \brief The Tomato class adds responsibilities to the core component Bun.
+ */
 class Tomato :public  Ingredient {
     public:
     Tomato();
