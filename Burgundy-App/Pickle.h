452bd4b6e616ef72186c5fb7976d02768908d95a
lerato15240 <147916325+lerato15240@users.noreply.github.com>
2023-11-06 21:59:59 +0200

    Update Pickle.h

--- Burgundy-App/Pickle.h
+++ Burgundy-App/Pickle.h
@@ -1,6 +1,10 @@
 #ifndef PICKLE_H
 #define PICKLE_H
 #include"Ingredient.h"
+/**
+ * \class Pickle
+ * \brief The pickle class adds responsibilities to the core component Bun.
+ */
 
 class Pickle :public Ingredient {
 
