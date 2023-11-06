7c6aac3897939873ac7019145cf3a81f9e512a73
lerato15240 <147916325+lerato15240@users.noreply.github.com>
2023-11-06 20:25:35 +0200

    doxygen comments update

--- Burgundy-App/DrinksChef.h
+++ Burgundy-App/DrinksChef.h
@@ -6,13 +6,29 @@
 #include"Water.h"
 #include"SoftDrink.h"
 #include"Drink.h"
-
+	/**
+	 * \class DrinksChef
+	 * \brief A concrete handler that can handle specific requests.
+	 */
 class DrinksChef: public Chef {
 
 public:
+       /**
+     * \DrinksChef constructor    
+     */
 	DrinksChef();
+	/**
+     * \brief returns next Handler    
+     */
 	Chef* getNext();
+	/**
+     * \brief sets next Handler   
+     */
 	void setNext(Chef* next);
+	/**
+     * \brief Handle the request if it's within this handler's capability, or pass it to the next handler.
+     * \param item The request to be handled.
+     */
 	void addOrderItem(Order* item);
 	
 };
