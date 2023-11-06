dbd0fa2ea473f2f4eec7d00df292a2760e6ee6aa
Siyamthanda Ndlovu <99127918+thunderain@users.noreply.github.com>
2023-11-07 00:21:48 +0200

    Update KitchenStaff.h Doxygen

--- Burgundy-App/KitchenStaff.h
+++ Burgundy-App/KitchenStaff.h
@@ -1,6 +1,9 @@
 #ifndef KITCHENSTAFF_H
 #define KITCHENSTAFF_H
-
+/**
+* \class KitchenStaff
+* \brief This abstract class is an interface for all chefs in the restaurant 
+*/
 class KitchenStaff {
   public:
   KitchenStaff();
