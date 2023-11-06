e6b6e262611410dc9943e86c64b6fb31b850bea7
Siyamthanda Ndlovu <99127918+thunderain@users.noreply.github.com>
2023-11-07 00:25:07 +0200

    Update Payment.h Doxygen

--- Burgundy-App/Payment.h
+++ Burgundy-App/Payment.h
@@ -1,9 +1,10 @@
 #ifndef PAYMENT_H
 #define PAYMENT_H
-
+/**
+* \class Payment
+* \brief This class is an interface for different strategies to pay the bill 
+*/
 class Payment {
-
-
 public:
 	void pay();
 };
