e811236dda62c1f9ab6824ff259f44c76fb6edd9
lerato15240 <147916325+lerato15240@users.noreply.github.com>
2023-11-06 21:53:59 +0200

    Update Patty.h

--- Burgundy-App/Patty.h
+++ Burgundy-App/Patty.h
@@ -1,7 +1,10 @@
 #ifndef PATTY_H
 #define PATTY_H
 #include"Ingredient.h"
-
+/**
+ * \class Patty
+ * \brief The Patty class adds responsibilities to the core component Bun.
+ */
 class Patty :public Ingredient {
 
 public:
