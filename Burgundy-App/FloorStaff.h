cbb7399f48791961eac5bb6aae723bb0641c9b86
Siyamthanda Ndlovu <99127918+thunderain@users.noreply.github.com>
2023-11-06 15:17:40 +0200

    Update FloorStaff.h Doxygen

--- Burgundy-App/FloorStaff.h
+++ Burgundy-App/FloorStaff.h
@@ -9,11 +9,23 @@ class Table;
 
 class FloorStaff {
 protected:
+	/**
+	 * \var Restaurant* restaurant
+	 * \brief This is a pointer to the restaurant being observed.
+	 */
 	Restaurant* restaurant;
+	/**
+	 * \var bool observerState
+	 * \brief This is the state of the restaurant.
+	 */
 	bool observerState;
 
 public:
 	FloorStaff();
+	/**
+	 * \brief Observes the satisfaction of the tables taken in as input
+	 * \param tables The list of tables to be observed.
+	 */
 	virtual void observeSatisfaction(std::vector<Table*>& tables)=0;
 };
 
