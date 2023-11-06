acc5b9968cfad0bea41150c20d51a6e61cd1eaf7
lerato15240 <147916325+lerato15240@users.noreply.github.com>
2023-11-06 19:52:28 +0200

    Doxygen comments update

--- Burgundy-App/Bun.h
+++ Burgundy-App/Bun.h
@@ -2,10 +2,18 @@
 #define BUN_H
 #include"Burger.h"
 #include"Bun.h"
-
+/**
+* \class Bun
+* \brief The base component that provides the core functionality
+*/
 class Bun : public Burger {
 
 public:
+   /**
+     * \brief Definition of of the addition of burger ingredients
+     *
+     * \param ingred The next Ingredient to be added
+     */
 	void addIngredient(Burger* ingred);
 };
 
