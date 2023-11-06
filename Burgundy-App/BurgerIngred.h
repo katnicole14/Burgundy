caf622bb8da053bd52668b2fdd1119ca593b6375
lerato15240 <147916325+lerato15240@users.noreply.github.com>
2023-11-06 19:54:04 +0200

    Doxygen comments update

--- Burgundy-App/BurgerIngred.h
+++ Burgundy-App/BurgerIngred.h
@@ -5,6 +5,11 @@ class BurgerIngred {
 
 
 public:
+  /**
+     * \brief Definition of  the addition of burger ingredients
+     *
+     * \param ingred The next Ingredient to be added
+     */
 	void addIngredient(BurgerIngred* ingred);
 };
 
