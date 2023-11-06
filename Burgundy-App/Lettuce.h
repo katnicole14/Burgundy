34674f5fa00a07621ff360d3a58cdaf24f2daf22
lerato15240 <147916325+lerato15240@users.noreply.github.com>
2023-11-06 21:59:17 +0200

    Update Lettuce.h

--- Burgundy-App/Lettuce.h
+++ Burgundy-App/Lettuce.h
@@ -1,7 +1,10 @@
 #ifndef LETTUCE_H
 #define LETTUCE_H
 #include"Ingredient.h"
-
+/**
+ * \class Lettuce
+ * \brief The Lettuce class adds responsibilities to the core component Bun.
+ */
 class Lettuce :public Ingredient {
 	public:
 	Lettuce();
