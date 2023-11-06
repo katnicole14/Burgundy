2fa48e2f92a16841bb3b18e4faefb64c6aec3832
Brenden1998 <50359806+Brenden1998@users.noreply.github.com>
2023-11-06 23:40:51 +0200

    Doxygen comments for BurgundySauce

--- Burgundy-App/BurgundySauce.h
+++ Burgundy-App/BurgundySauce.h
@@ -1,10 +1,19 @@
 #ifndef BURGUNDYSAUCE_H
 #define BURGUNDYSAUCE_H
-
+/**
+* \class BurgundySauce
+* \brief This class adds responsibilities to the core component Bun.
+*/
 class BurgundySauce {
 
 public:
+	/**
+	* \brief The constructor of the BurgundySauce class.
+	*/
 	BurgundySauce();
+	/**
+	* \brief Prints out that sauce was added to the burger.
+	*/
 	void print();
 };
 
