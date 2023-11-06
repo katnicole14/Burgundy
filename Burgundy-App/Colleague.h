35014f52f9a825d063760ca83206caf8625680ea
Brenden1998 <50359806+Brenden1998@users.noreply.github.com>
2023-11-06 16:50:20 +0200

    Doxygen comments for Colleague

--- Burgundy-App/Colleague.h
+++ Burgundy-App/Colleague.h
@@ -2,16 +2,31 @@
 #define COLLEAGUE_H
 
 class Order;
-
+/**
+* \class Colleague
+* \brief An abstract class that defines methods for other classes to implement.
+* Defines the functions that allow the table's customers to place their orders and receive their meals. Also defines functions that allow
+* the head chef to receive customers' orders from the waiter and to send out the finished meals from the kitchen.
+*/
 class Colleague {
 public:
 //    Colleague();
 //    ~Colleague();
-
+    /**
+    * \brief Allows the head chef to receive customer orders from the waiter.
+    */
     virtual void receiveOrder(Order* order) = 0; // HeadChef
+    /**
+    * \brief Allows the head chef to send the finished meals to the waiter.
+    */
     virtual Order* sendOutFinishedMeal() = 0; // HeadChef
-
+    /**
+    * \brief Allows the table to place their orders so the waiter can pass it to the kitchen.
+    */
     virtual void placeOrders() = 0; // Table
+    /**
+    * \brief Allows the table to receive their finished meals from the waiter.
+    */
     virtual void receiveFinishedMeal(Order* order) = 0; // Table
 };
 
