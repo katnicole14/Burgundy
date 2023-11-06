436798346cbe0a93ee0eadfba6b1258145bea4fa
lerato15240 <147916325+lerato15240@users.noreply.github.com>
2023-11-06 21:52:14 +0200

    Update Water.h

--- Burgundy-App/Water.h
+++ Burgundy-App/Water.h
@@ -1,7 +1,10 @@
 #ifndef WATER_H
 #define WATER_H
 #include "Drink.h"
-
+/**
+ * \class Water
+ * \brief The ConcreteDecoratorA class adds responsibilities to the core component Bun.
+ */
 class Water : public Drink {
 	public:
 		Water();
