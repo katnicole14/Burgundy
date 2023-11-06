5d0308cd335eb30f09a965b36eefec0417d1973e
Siyamthanda Ndlovu <99127918+thunderain@users.noreply.github.com>
2023-11-07 00:03:16 +0200

    Update SplitBlill.h Doxygen

--- Burgundy-App/SplitBlill.h
+++ Burgundy-App/SplitBlill.h
@@ -1,6 +1,10 @@
 #ifndef SPLITBLILL_H
 #define SPLITBLILL_H
 #include "Payment.h"
+/**
+* \class SplitBill
+* \brief This class is a strategy to pay the bill by splitting it
+*/
 class SplitBlill : Payment {
 
 
