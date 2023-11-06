778436d53ae89e9f31ef47e057fe8d272e5db761
Brenden1998 <50359806+Brenden1998@users.noreply.github.com>
2023-11-06 15:59:45 +0200

    Doxygen comments for Manager

--- Burgundy-App/Manager.h
+++ Burgundy-App/Manager.h
@@ -5,14 +5,27 @@
 #include "Table.h"
 
 #include "AbstractBuilder.h"
-
+/**
+* \class Manager
+* \brief The Manager will construct tables for customers to sit at and also observe customer satisfaction.
+*/
 class Manager : public FloorStaff {
 private:
+	/**
+ 	* \brief A reference of an AbstractBuilder object.
+  	*/
     AbstractBuilder* builder;
 
 public:
-
+	/**
+	* \brief The constructor for the Manager class.
+ 	* \param b A Builder object that the Manager will use to build tables.
+ 	*/
     Manager(AbstractBuilder* b) : builder(b) {}
+	/**
+	* \brief Calls the buildPart function on the builder reference object.
+ 	* \param numCustomers The number of customers entering the restaurant.
+ 	*/
     void construct(int numCustomers);
 	/**
 	 * \brief Observes the satisfaction of the tables taken in as input
