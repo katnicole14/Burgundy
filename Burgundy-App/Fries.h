6bc54e6a90526a171b5e20daa1479e2e68c33550
lerato15240 <147916325+lerato15240@users.noreply.github.com>
2023-11-06 20:29:30 +0200

    Doxygen comments update

--- Burgundy-App/Fries.h
+++ Burgundy-App/Fries.h
@@ -1,10 +1,20 @@
 #ifndef FRIES_H
 #define FRIES_H
+	/**
+	 *\class Fries
+        * \brief adds responsibility to the Bun components  
+        */
 
 class Fries {
 
 public:
+	/**
+     	* \brief Fries constructor  
+     	*/
 	Fries();
+	/**
+     	* \brief prints out fries     
+     	*/
 	void print();
 };
 
