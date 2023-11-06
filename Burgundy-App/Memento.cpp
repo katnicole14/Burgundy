de225e27a91acfee45e02e4cb34cf7becc412e9e
SechabaNtshehi <146662908+SechabaNtshehi@users.noreply.github.com>
2023-11-06 12:32:11 +0200

    Added doxygen comments for the Memento object

--- Burgundy-App/Memento.cpp
+++ Burgundy-App/Memento.cpp
@@ -2,6 +2,15 @@
 
 using namespace std;
 
+/*! \brief A summary of the Memento class.
+ * 		The Memento takes a snapshot of the state of the Order object then stores it for later use. Details to follow.
+ * 
+ * The Memento stores the state of the Order object, more specifically, the orders made by the customers at the table, the tableID
+ * and the number of customers at the table. The Memento class makes use of a State class to store the snapshot.
+ */
+
+/// Stores the snapshot into the State object
+
 Memento::Memento(int*** orderArray_, int tableID_, int numCustomers_){
 	state= new State(orderArray_, tableID_, numCustomers_);
 }
