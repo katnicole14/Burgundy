35a8fa29c7e7c9d23d483ea40394ba2e24e10a4c
lerato15240 <147916325+lerato15240@users.noreply.github.com>
2023-11-06 21:42:26 +0200

    doxygen comments updates

--- Burgundy-App/FriesChef.h
+++ Burgundy-App/FriesChef.h
@@ -4,13 +4,30 @@
 #include"Fries.h"
 #include"Order.h"
 class Fries;
-
+/**
+ * \class FriesChef
+ * \brief A concrete handler that can handle specific requests.
+ */
 class FriesChef : public Chef {
 
 public:
+	/**
+	* \brief FriesChef constructor  
+	*/
 	FriesChef (Chef* next);
+	/**
+	* \brief return the next handler in the chain    
+	*/
 	Chef* getNext();
+	/**
+	* \brief sets the next handler in the chain    
+	*/
 	void setNext(Chef* next);
+	/**
+	* 
+	* \brief Handle the request if it's within this handler's capability, or pass it to the next handler.
+	* \param item The request to be handled.
+	*/
 	void addOrderItem(Order* item);
 	
 };
